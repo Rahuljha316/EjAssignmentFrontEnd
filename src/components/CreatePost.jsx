e92bbafbdcf2f3c50dde104db5d1fbd431220e5d
Rahul Jha <rahulbudy11@gmail.com>
2023-03-23 20:18:07 +0530

    initialised the frontend project

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,24 @@
+const CreatePost =() =>{
+    return(
+        <div>
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>content</th>
+                    <th>createdAt</th>
+                    <th>UpdatedAt</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>something</td>
+                    <td>something</td>
+                    <td>1598</td>
+                    <td>34</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default CreatePost;
---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+const Home =() =>{
+    return(
+        <div>
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>content</th>
+                    <th>createdAt</th>
+                    <th>UpdatedAt</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>something</td>
+                    <td>something</td>
+                    <td>1598</td>
+                    <td>34</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default Home;
